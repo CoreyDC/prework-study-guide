ece6692d3138ec0aac3fa3ea081ac8c3ef7fa26e
Corey Calbreath <corey_calbreath@yahoo.com>
2022-11-27 18:15:24 -0600

    Adding functions to JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,32 @@
 var topics = ['HTML', 'Git', 'CSS', 'JavaScript']
 
-for ( let i = 0; i < topics.length; i++ ) {
+var randomTopics = topics[ Math.floor( Math.random() * topics.length ) ]
+
+function listTopics() {
+    for ( let i = 0; i < topics.length; i++ ) {
     console.log(topics[i]);
+    }
 }
 
-/*
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
+function selectTopic() {
+    if (randomTopics === 'HTML') {
+    console.log("Let's study HTML!");
+    } else if (randomTopics === 'CSS') {
+    console.log("Let's study CSS!");
+    } else if (randomTopics === 'Git') {
+    console.log("Let's study Git!");
+    } else if (randomTopics === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+    } else {
+    console.log('Please try again!');
+    }
 }
-*/
+
+console.log('Here are the topics we learned through prework:');
+listTopics();
+
+console.log('Which topic should we study first?');
+selectTopic();
+
+
+
